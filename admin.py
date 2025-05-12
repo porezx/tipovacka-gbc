2b1de56937304a9aba4aca218c8cf099a70f2f0f
porezx <honza.porizka@gmail.com>
2025-05-12 21:39:36 +0200

    Popis změn, třeba: opravena validace v loginu

--- admin.py
+++ admin.py
@@ -1,15 +1,19 @@
-# create_admin.py
-
 from main import app
 from extensions import db
 from models import User
 
 with app.app_context():
-    existing = User.query.filter_by(username='admin').first()
-    if existing:
-        print("❌ Uživatel 'admin' už existuje.")
-    else:
-        user = User(username='admin', secret_word='tajne', is_admin=True)
-        db.session.add(user)
-        db.session.commit()
-        print("✅ Admin uživatel 'admin' byl úspěšně vytvořen.")
+    # smažeme starého admina (pokud existuje)
+    User.query.filter_by(username='admin').delete()
+    db.session.commit()
+
+    # vytvoříme nového
+    user = User(
+        username='admin',
+        secret_word='tajne',
+        is_admin=True,
+        custom_id=1
+    )
+    db.session.add(user)
+    db.session.commit()
+    print("✅ Admin uživatel 'admin' byl úspěšně vytvořen.")
